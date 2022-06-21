cc9ac51db95dc1b8e5c22b50675f9ecc864dc3d7
Andrew Thomas <Andrew.J.Thomas@du.edu>
2022-06-21 10:34:39 -0600

    finalize etherscan network sampling

--- src/extraction/main.py
+++ src/extraction/main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import requests
-import networkx as nx
+import datetime
+from tqdm import tqdm
 
 API_KEY = 'ZW8EDBWKP1EI6DAW7TN567JY17K69DJYCR'
 INIT_ADDRESS = '0xff0bd4aa3496739d5667adc10e2b843dfab5712b'
@@ -16,7 +17,7 @@ class GetEthereumGraph:
         """
         self.api_key = api_key
         self.processed_addresses = [init_address]
-        self.graph = self._get_social_graph(init_address, self.api_key)
+        self.transactions = self._get_ethereum_transactions(init_address, self.api_key)
 
     def _get_ethereum_transactions(self, address, api_key):
         """
@@ -35,30 +36,24 @@ class GetEthereumGraph:
         transactions = requests.get(url, params=params)
         return pd.json_normalize(transactions.json(), record_path=["result"])
 
-    def _get_social_graph(self, address, api_key):
-        """
-        Returns the social network based on the list of transactions
-        :param address: Ethereum address
-        :param api_key: Etherscan API key
-        :return: netowrkx.graph
-        """
-        data = self._get_ethereum_transactions(address, api_key)
-        return nx.from_pandas_edgelist(data, source="from", target="to", edge_attr=True, create_using=nx.DiGraph())
-
     def add_step(self):
         """
         Loops through every address and appends the graph of those transactions to the main graph.
-        :return: networkx.graph
+        :return: pandas.DataFrame
         """
-        for a in self.graph.nodes:
+        addresses = self.transactions["from"].append(self.transactions["to"]).unique()
+        for i in tqdm(range(len(addresses))):
+            a = addresses[i]
             if a not in self.processed_addresses:
-                print(f"Address currently processing: {a}")
-                self.graph = nx.union(self.graph,
-                                      self._get_social_graph(a, self.api_key))
+                self.transactions = pd.concat(
+                    [self.transactions,
+                     self._get_ethereum_transactions(a, self.api_key)],
+                    axis=1
+                )
                 self.processed_addresses = self.processed_addresses + [a]
+        self.transactions = self.transactions.drop_duplicates()
 
 
 if __name__ == "__main__":
     ether_graph = GetEthereumGraph(INIT_ADDRESS)
     ether_graph.add_step()
-    nx.draw(ether_graph.graph)
